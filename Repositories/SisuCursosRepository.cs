b8868a9db826533e4e95e80f801ad0a8231d5c43
gabriellourencao <131073630+gabriellourencao@users.noreply.github.com>
2024-11-19 07:40:09 -0300

    Criação dos repositories do Sisu
    Criação dos repositories SisuCursos e SisuModalidades

---
+++ Repositories/SisuCursosRepository.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using Extensionista.Models;
+
+namespace Extensionista.Repositories
+{
+    public class SisuCursosRepository
+    {
+        private readonly SQLiteConnection _connection;     
+
+        public SisuCursosRepository()
+        {
+            _connection = DataBaseContext.connection;
+        }
+
+        public List<SisuCursos> ObterCursosSisu(String CodigoIES)
+        {
+            var SisuCursos = _connection.Table<SisuCursos>()
+                                          .Where(u => u.Codigo_Ies == CodigoIES)
+                                          .ToList();
+
+            return SisuCursos;
+        }
+
+        public List<SisuCursos> ObterCursoSisu(String CodigoIES, String IdCurso)
+        {
+            var SisuCursos = _connection.Table<SisuCursos>()
+                                        .Where(u => u.Codigo_Ies == CodigoIES && u.Id_Curso == IdCurso)                                        
+                                        .ToList();
+            return SisuCursos;
+        }     
+     
+    }
+}
