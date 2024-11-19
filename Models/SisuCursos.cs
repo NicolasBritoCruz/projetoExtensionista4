ad802c5208533f399380e6f97c1121d0ab220265
gabriellourencao <131073630+gabriellourencao@users.noreply.github.com>
2024-11-19 07:38:56 -0300

    Criação dos models do Sisu
    Criação dos models SisuCursos e SisuModalidades

---
+++ Models/SisuCursos.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Extensionista.Models
+{
+    public class SisuCursos
+    {
+        [Key]
+        public string Id_Curso { get; set; } = string.Empty;
+
+        public string Codigo_Ies { get; set; } = string.Empty;
+        public string Nome_Curso { get; set; } = string.Empty;
+        public int Qt_Vagas { get; set; } = 0;
+        public string Turno { get; set; } = string.Empty;
+        public string Nome_Ies { get; set; } = string.Empty;
+        public string Sigla_Ies { get; set; } = string.Empty;
+        public string Uf { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+        public float Peso_Cn { get; set; } = 0.0f;
+        public float Peso_Ch { get; set; } = 0.0f;
+        public float Peso_L { get; set; } = 0.0f;
+        public float Peso_M { get; set; } = 0.0f;
+        public float Peso_R { get; set; } = 0.0f;
+        public string Site_Ies { get; set; } = string.Empty;
+    }
+}
---
+++ Models/SisuModalidades.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Extensionista.Models
+{
+    public class SisuModalidades
+    {
+        public string Id_Curso { get; set; } = string.Empty;
+        public string Tipo_Concorrencia { get; set; } = string.Empty;
+        public float Nota_Corte_2024 { get; set; } = 0.0f;
+        public float Nota_Corte_2023_1 { get; set; } = 0.0f;
+        public float Nota_Corte_2023_2 { get; set; } = 0.0f;
+        public int Qt_Vagas { get; set; } = 0;
+
+        [ForeignKey(nameof(Id_Curso))]
+        public SisuCursos Curso { get; set; } = null!;
+    }
+}
