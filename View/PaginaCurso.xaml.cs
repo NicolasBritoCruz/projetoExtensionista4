ad83cda9809f00cbc647730404401ec58eb4e6ec
NicolasBritoCruz <cruznicolasbrito@gmail.com>
2024-11-27 20:15:45 -0300

    Implementação do Gráfico de Vagas

--- View/PaginaCurso.xaml.cs
+++ View/PaginaCurso.xaml.cs
@@ -31,6 +31,18 @@ public partial class PaginaCurso : ContentPage
 
     private async void IndicadoresButton_Clicked(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new PaginaIndicadores());
+        if (sender is Element element && element.BindingContext is SisuCursos SelectedCurso)
+        {
+            try
+            {
+                string IdCurso = SelectedCurso.ID_CURSO;
+                await Navigation.PushAsync(new PaginaIndicadores(IdCurso));
+            }
+            catch (Exception ex)
+            {
+                // Log ou mensagem de erro para o usuário
+                await DisplayAlert("Erro", $"Ocorreu um erro ao verificar o SISU: {ex.Message}", "OK");
+            }
+        }
     }
 }
--- View/PaginaIndicadores.xaml.cs
+++ View/PaginaIndicadores.xaml.cs
@@ -1,15 +1,34 @@
+using Extensionista.Drawables;
+using Extensionista.Repositories;
+
 namespace Extensionista.View;
 
 public partial class PaginaIndicadores : ContentPage
 {
-	public PaginaIndicadores()
-	{
-		InitializeComponent();
+    public PaginaIndicadores(string IdCurso)
+    {
+        InitializeComponent();
         NavigationPage.SetHasNavigationBar(this, false);
+
+        // Obter os dados do repositório
+        var repository = new SisuModalidadesRepository();
+        var modalidades = repository.ObterCursosPorIdCurso(IdCurso);
+
+        // Configurar o drawable
+        var drawable = new GraficoBarrasDrawable
+        {
+            Modalidades = modalidades, // Atribuir diretamente
+            Rotulos = new List<string> { "AC", "RaReEs", "RaEs", "Es", "DeEs", "DeReEs", "ReEs", "?", "De" }
+        };
+
+        // Associar ao GraphicsView e atualizar
+        BarrasGrafico.Drawable = drawable;
+        BarrasGrafico.Invalidate();
     }
+
 
     private async void sairIndicadores_Clicked(object sender, EventArgs e)
     {
-		await Navigation.PopAsync();
+        await Navigation.PopAsync();
     }
 }
